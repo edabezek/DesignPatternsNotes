9ed3440d9043cff7a3b20e7923ae05464b5b6e24
eda.bezek <eda_bezek@hotmail.com>
2022-09-25 20:45:44 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            //yapılacak işlemler
+            customerManager.Save();
+        }
+    }
+    class CustomerManager//constructor'u olan ama dışarıdan erişilmeyen
+    {
+        private static CustomerManager _instance;   
+        private CustomerManager()
+        {
+                
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            if (_instance == null)//daha önce CustomerManager instance'ı oluşturulmamışsa,bossa oluştur. 
+            {
+                _instance = new CustomerManager();  
+            }
+            return _instance;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+}
---
+++ ThreadSafeSingleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ThreadSafeSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            //yapılacak işlemler
+            customerManager.Save();
+        }
+    }
+    class CustomerManager//constructor'u olan ama dışarıdan erişilmeyen
+    {
+        private static CustomerManager _instance;
+        static object _Lock = new object();
+        private CustomerManager()
+        {
+
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_Lock)
+            {
+                if (_instance == null)//daha önce CustomerManager instance'ı oluşturulmamışsa,bossa oluştur. 
+                {
+                    _instance=new CustomerManager();
+                }
+            }            
+            return _instance;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+}
