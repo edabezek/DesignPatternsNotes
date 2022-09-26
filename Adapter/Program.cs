37e48b78419bf0323ad973f5db7b5722fafdffaf
eda.bezek <eda_bezek@hotmail.com>
2022-09-26 20:16:36 +0300

    Adapter design

---
+++ Adapter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Adapter//mesela kimlik paylaşım sistemini dahil edeceğiz yada servisimiz varsa onu dahil ederken kullanılır
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EdLogger());
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+    class ProductManager
+    {
+        private ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data");
+            Console.WriteLine("Saved");
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class EdLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}",message);
+        }
+    }
+    //Nuıgetten indirdik diyelim-kısacası dll değiştiremiyoruz
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged, {0}", message);
+        }
+    }
+    class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
---
+++ CompositeDesign/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompositeDesign//bir kurumda çalışanlar için model 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface IPerson
+    {
+        string Name { get; set; }   
+    }
+    class Employee : IPerson,IEnumerable<IPerson> //foreach ile nesneleri gezebileceğimiz bir yapı IEnumerable
+    {
+        List<IPerson> _subordinates=new List<IPerson>();     //_subordinates hiyerarşide en alt
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);  
+        }
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+        public IPerson GetSubordinate(int index)
+        {
+            return _subordinates[index];    
+        }
+
+        public string Name { get ; set ; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;    
+            }   
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
